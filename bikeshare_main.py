7dc56236d7635fd91021b5368d98099a4fc3b96b
Gladston Joseph <gladyjoseph@gmail.com>
2019-02-11 10:39:11 +0530

    Code updated according to pep8 Guidelines

--- bikeshare_main.py
+++ bikeshare_main.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import datetime
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = {'chicago': 'chicago.csv',
+             'new york city': 'new_york_city.csv',
+             'washington': 'washington.csv'}
 
-month_choose_list = {"january": 1, 
+month_choose_list = {"january": 1,
                      "february": 2,
                      "march": 3,
                      "april": 4,
@@ -26,6 +26,7 @@ day_choose_list = {"monday": 0,
 
 restart_choose_list = ["yes", "no"]
 
+
 def user_input_check(user_input, choose_list, category):
     base_text = "Please choose "
     while user_input not in choose_list:
@@ -34,7 +35,7 @@ def user_input_check(user_input, choose_list, category):
         elif category == "month":
             user_input = input(base_text + "a valid month! : ").lower()
         elif category == "day_of_week":
-            user_input = input(base_text + "a valid day of the week! : ").lower()
+            user_input = input(base_text + "a valid day of the week! : ").lower()  # NOQA
         elif category == "yes_or_no":
             user_input = input(base_text + "either 'yes' or 'no' : ").lower()
 
@@ -46,25 +47,32 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        1. (str) city - name of the city to analyze
+        2. (str) month - name of the month to filter by, or "all" to apply no
+        month filter
+        3. (str) day - name of the day of week to filter by, or "all" to apply
+        no day filter
     """
 
     print('Hello! Let\'s explore some US bikeshare data!\n')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    user_city_input = input("Which city would you like to analyze ? \nChoose chicago, new york city, washington : ").lower()
+
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs  # NOQA
+    user_city_input = input("Which city would you like to analyze ? \nChoose chicago, new york city, washington : ").lower()  # NOQA
     user_city_input = user_input_check(user_city_input, CITY_DATA, "city")
     city = CITY_DATA[user_city_input]
 
     # get user input for month (all, january, february, ... , june)
-    user_month_input = input("\nWhich month would you like to analyze ? \nChoose any month from january to june. Choose 'all' if you want to see every month : ").lower()
-    user_month_input = user_input_check(user_month_input, month_choose_list, "month")
+    user_month_input = input("\nWhich month would you like to analyze ? \nChoose any month from january to june. Choose 'all' if you want to see every month : ").lower()  # NOQA
+    user_month_input = user_input_check(user_month_input,
+                                        month_choose_list,
+                                        "month")
     month = month_choose_list[user_month_input]
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    user_day_input = input("\nWhich day of the week would you like to analyze ? \nChoose any day of the week. Choose 'all' if you want to see all : ").lower()
-    user_day_input = user_input_check(user_day_input, day_choose_list, "day_of_week")
+    user_day_input = input("\nWhich day of the week would you like to analyze ? \nChoose any day of the week. Choose 'all' if you want to see all : ").lower()  # NOQA
+    user_day_input = user_input_check(user_day_input,
+                                      day_choose_list,
+                                      "day_of_week")
     day = day_choose_list[user_day_input]
 
     print("\n" + '-'*40)
@@ -73,12 +81,15 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day if
+    applicable.
 
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        1. (str) city - name of the city to analyze
+        2. (str) month - name of the month to filter by, or "all" to apply no
+        month filter
+        3. (str) day - name of the day of week to filter by, or "all" to apply
+        no day filter
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
@@ -95,7 +106,7 @@ def load_data(city, month, day):
     if day != "all":
         df = df[df["day_of_week"] == day]
 
-    df = df.reset_index(drop = True)
+    df = df.reset_index(drop=True)
     return df, city, month, day
 
 
@@ -112,7 +123,7 @@ def time_stats(df, city, month, day):
 
         for key, value in month_choose_list.items():
             if value == common_month[0]:
-                print("The most common month is {}. A total of {} trips were made".format(key, common_month_count))
+                print("The most common month is {}. A total of {} trips were made".format(key, common_month_count))  # NOQA
 
     # display the most common day of week
     if day == "all":
@@ -121,21 +132,21 @@ def time_stats(df, city, month, day):
 
         for key, value in day_choose_list.items():
             if value == common_day[0]:
-                print("The most common day of the week is {}. A total of {} trips were made".format(key, common_day_count))
+                print("The most common day of the week is {}. A total of {} trips were made".format(key, common_day_count))  # NOQA
 
     # display the most common start hour
     common_starthour = df["Start Time"].dt.hour.mode()
     common_starthour_count = df["Start Time"].dt.hour.value_counts().max()
-    print("The most common starting hour of the trip is the {}th hour. A total of {} trips were made".format(common_starthour[0], common_starthour_count))
+    print("The most common starting hour of the trip is the {}th hour. A total of {} trips were made".format(common_starthour[0], common_starthour_count))  # NOQA
 
     # display the most common end hour
     common_endhour = df["End Time"].dt.hour.mode()
     common_endhour_count = df["End Time"].dt.hour.value_counts().max()
-    print("The most common ending hour of the trip is the {}th hour. A total of {} trips were made".format(common_endhour[0], common_endhour_count))
+    print("The most common ending hour of the trip is the {}th hour. A total of {} trips were made".format(common_endhour[0], common_endhour_count))  # NOQA
 
     # display the average duration of all trips
     average_duration = df["Trip Duration"].mean() / 60
-    print("The average duration of the trips is {} minutes".format(round(average_duration)))
+    print("The average duration of the trips is {} minutes".format(round(average_duration)))  # NOQA
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -150,19 +161,19 @@ def station_stats(df):
     # display most commonly used start station
     common_startstation = df["Start Station"].mode()
     common_startstation_count = df["Start Station"].value_counts().max()
-    print("The most common starting station is {}. A total of {} trips were made".format(common_startstation[0], common_startstation_count))
+    print("The most common starting station is {}. A total of {} trips were made".format(common_startstation[0], common_startstation_count))  # NOQA
 
     # display most commonly used end station
     common_endstation = df["End Station"].mode()
     common_endstation_count = df["End Station"].value_counts().max()
-    print("The most common ending station is {}. A total of {} trips were made".format(common_endstation[0], common_endstation_count))
+    print("The most common ending station is {}. A total of {} trips were made".format(common_endstation[0], common_endstation_count))  # NOQA
 
     # display most frequent combination of start station and end station trip
-    journeys_df = df["Start Station"].str.cat(df["End Station"], sep = "__")
+    journeys_df = df["Start Station"].str.cat(df["End Station"], sep="__")
     common_journey = journeys_df.mode()[0].split("__")
     common_journey_count = journeys_df.value_counts().max()
 
-    print("The most common trips start from {} and end at {}. A total of {} trips were made".format(common_journey[0], common_journey[1], common_journey_count))
+    print("The most common trips start from {} and end at {}. A total of {} trips were made".format(common_journey[0], common_journey[1], common_journey_count))  # NOQA
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -176,11 +187,11 @@ def trip_duration_stats(df):
 
     # display total travel time
     total_travel_time = df["Trip Duration"].sum() / (60 * 60)
-    print("The total travel time is {} hours".format(int(round(total_travel_time))))
+    print("The total travel time is {} hours".format(int(round(total_travel_time))))  # NOQA
 
     # display mean travel time
     average_travel_time = df["Trip Duration"].mean() / 60
-    print("The average travel time per trip is {} minutes".format(int(round(average_travel_time))))
+    print("The average travel time per trip is {} minutes".format(int(round(average_travel_time))))  # NOQA
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -210,7 +221,7 @@ def user_stats(df):
         print("The number of Male & Female bikers are :\n")
 
         for gender_index in gender_types:
-            print("{}: {}".format(gender_index, gender_type_counts[gender_index]))
+            print("{}: {}".format(gender_index, gender_type_counts[gender_index]))  # NOQA
 
         print("\n")
 
@@ -230,27 +241,32 @@ def user_stats(df):
                 elif df["Gender"][index].lstrip().rstrip().lower() == "female":
                     gender_name = "She"
 
-                print("The {} person to bike is {} years old. ".format(category, age) + gender_name + " was born in {}".format(year))
+                print("The {} person to bike is {} years old. ".format(category, age) + gender_name + " was born in {}".format(year))  # NOQA
             else:
-                print("The {} person to bike is {} years old. ".format(category, age) + gender_name + " was born in {}".format(year))
+                print("The {} person to bike is {} years old. ".format(category, age) + gender_name + " was born in {}".format(year))  # NOQA
 
         # data for the earliest year
         earliest_year = df["Birth Year"].min()
         oldest_age = get_age(earliest_year)
         earliest_year_index = df["Birth Year"].idxmin()
-        #print(df["Gender"][earliest_year_index])
-        print_user_stats(int(earliest_year), oldest_age, earliest_year_index, "oldest")
+        # print(df["Gender"][earliest_year_index])
+        print_user_stats(int(earliest_year),
+                         oldest_age, earliest_year_index,
+                         "oldest")
 
         # data for the latest year
         latest_year = df["Birth Year"].max()
         youngest_age = get_age(latest_year)
         latest_year_index = df["Birth Year"].idxmax()
-        print_user_stats(int(latest_year), youngest_age, latest_year_index, "youngest")
+        print_user_stats(int(latest_year),
+                         youngest_age,
+                         latest_year_index,
+                         "youngest")
 
         # data for the most common
         most_common_year = df["Birth Year"].mode()[0]
         most_common_age = get_age(most_common_year)
-        print("The most common age for biking is {}. The person was likely born in {}".format(most_common_age, int(most_common_year)))
+        print("The most common age for biking is {}. The person was likely born in {}".format(most_common_age, int(most_common_year)))  # NOQA
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -268,18 +284,22 @@ def main():
 
 def raw_trip_data(df, check):
     if check == 0:
-        user_choice_raw_trip = input("\nWould you like to view individual trip data ? : ").lower()
-        user_choice_raw_trip = user_input_check(user_choice_raw_trip, restart_choose_list, "yes_or_no")
+        user_choice_raw_trip = input("\nWould you like to view individual trip data ? : ").lower()  # NOQA
+        user_choice_raw_trip = user_input_check(user_choice_raw_trip,
+                                                restart_choose_list,
+                                                "yes_or_no")
     elif check > 0:
-        user_choice_raw_trip = input("\nWould you like to see the next six rows ? : ").lower()
-        user_choice_raw_trip = user_input_check(user_choice_raw_trip, restart_choose_list, "yes_or_no")
+        user_choice_raw_trip = input("\nWould you like to see the next six rows ? : ").lower()  # NOQA
+        user_choice_raw_trip = user_input_check(user_choice_raw_trip,
+                                                restart_choose_list,
+                                                "yes_or_no")
 
     show_raw_trip_data(df, user_choice_raw_trip, check)
 
 
 def show_raw_trip_data(df, user_choice, check):
     if user_choice == "yes":
-        print(df.ix[check : check + 5])
+        print(df.ix[check: check + 5])
         check = check + 5
         raw_trip_data(df, check)
     else:
@@ -296,8 +316,8 @@ def restart_program():
     if user_choice == "yes":
         main()
     else:
-        print("\nHope you liked my programming skills ! \nMade with passion, by Gladston\n")
+        print("\nHope you liked my programming skills ! \nMade with passion, by Gladston\n")  # NOQA
         exit()
 
 if __name__ == "__main__":
-	main()
+    main()
